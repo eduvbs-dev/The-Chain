ff8bc1e0dde80a20861ac795e31c86a0b09b041c
eduvbs-dev <edubgamer331@gmail.com>
2026-08-28 21:17:35 -0300

    Primeiro Commit

---
+++ meiasrangel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+int n, valor;
+scanf("%d", &n);
+valor=n+1;
+printf("%d", valor);
+
+    return 0;
+}
---
+++ notaesquecida.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+int a, b, M;
+scanf("%d %d", &a, &M);
+b=2*M-a;
+printf("%d\n", b);
+
+    return 0;
+}
